9eef2e6c1b2c330763989b81485ea1d07ec84efb
themango314 <47112779+themango314@users.noreply.github.com>
2019-01-29 10:54:30 +0100

    Premiere modification_premier commit
    Modification identique au pdf

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,14 +1,28 @@
 package com.example;
 
-
+/**
+ *This is a class.
+ */
 public class Greeter {
-
+ 
+  /**
+   * This is a constructor.
+   */
+  public Greeter ()     
   
-  public Greeter() {
+}
 
-  }
-
-  public final String greet(final String someone) {
-    return String.format("HelloMango, %s!", someone);
+/**
+ * @param name of a person 
+ * @return greeting string 
+ */
+Public final String greet(final String someone) {
+    return String.format("HelloMango, %s!", someone); 
   }
 }
+ 
+  
+ 
+ 
+
+  
